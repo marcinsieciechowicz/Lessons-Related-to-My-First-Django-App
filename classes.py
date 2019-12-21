11768403453ecf9d7d636f497b36f86ed516aa8e
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2019-12-21 14:49:47 +0100

    classes lessons upgrading

--- classes.py
+++ classes.py
@@ -1,3 +1,28 @@
+import abc
+from abc import ABC, abstractclassmethod
+
+
+class AbstractOperation(ABC):
+
+    def __init__(self, operand_a, operand_b):
+        self.operand_a = operand_a
+        self.operand_b = operand_b
+        super(AbstractOperation, self).__init__()
+
+    @classmethod
+    @abc.abstractmethod
+    def do_something(self):
+        pass
+
+
+class ConcreteOperation(AbstractOperation):
+    pass
+
+
+class AddOperation(AbstractOperation):
+    def execute(self):
+        return
+
 class Fam_member:
     def __init__(self, surname):
         self.l_name = surname
@@ -17,6 +42,7 @@ class Nicolas(Fam_member):
     def full_name(self):
         print(self.f_name, self.l_name)
 
+
 nico = Nicolas('Sieciechowicz', 'Mikolaj')
 nico.surname()
 nico.name()
@@ -24,3 +50,5 @@ nico.full_name()
 fam_mem = Fam_member('Sieciechowicz')
 fam_mem.surname()
 
+# learn about BUILT-IN FUNCTIONS from python.docs, 1 per day (there are about 70), also pythoninstitute (1 hour/day) making a course,
+# watch youtube films from user (added to favourites)
---
+++ name_main/main.py
@@ -0,0 +1,3 @@
+import imp as imp2
+
+print(__name__)
