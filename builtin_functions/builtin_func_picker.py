b7dcadb5118c224790d120096593c36e320efab7
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2019-12-25 00:47:30 +0100

    start of using the programme

---
+++ builtin_functions/builtin_func_picker.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect('db')
+with conn:
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    sql = 'SELECT name FROM builtin_funcs WHERE learned_on IS NULL ORDER BY RANDOM() LIMIT 1 '
+    cur.execute(sql)
+    row = cur.fetchone()
+    random_builtin_func = row['name']
+    url = 'https://docs.python.org/3/library/functions.html#' + random_builtin_func.replace('()', '')
+    print(url)
+    sql = 'UPDATE builtin_funcs SET learned_on = DATE() WHERE name = ?'
+    cur.execute(sql, [random_builtin_func])
