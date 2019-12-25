411aa66c637fbac85504937d014a4d051d3d0a90
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2019-12-25 01:40:35 +0100

    some fix

--- builtin_functions/builtin_func_picker.py
+++ builtin_functions/builtin_func_picker.py
@@ -1,27 +1,15 @@
-# import sqlite3
-#
-# conn = sqlite3.connect('db')
-# with conn:
-#     conn.row_factory = sqlite3.Row
-#     cur = conn.cursor()
-#     sql = 'SELECT name FROM builtin_funcs WHERE learned_on IS NULL ORDER BY RANDOM() LIMIT 1 '
-#     cur.execute(sql)
-#     row = cur.fetchone()
-#     random_builtin_func = row['name']
-#     url = 'https://docs.python.org/3/library/functions.html#' + random_builtin_func.replace('()', '')
-#     print(url)
-#     sql = 'UPDATE builtin_funcs SET learned_on = DATE() WHERE name = ?'
-#     cur.execute(sql, [random_builtin_func])
+import sqlite3
 
-numlist = ['12', '13', '14', '15']
-separator = ', '
-print(separator.join(numlist))
+conn = sqlite3.connect('db')
+with conn:
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    sql = 'SELECT name FROM builtin_funcs WHERE learned_on IS NULL ORDER BY RANDOM() LIMIT 1 '
+    cur.execute(sql)
+    row = cur.fetchone()
+    random_builtin_func = row['name']
+    url = 'https://docs.python.org/3/library/functions.html#' + random_builtin_func.replace('()', '')
+    print(url)
+    sql = 'UPDATE builtin_funcs SET learned_on = DATE() WHERE name = ?'
+    cur.execute(sql, [random_builtin_func])
 
-numtuple = ('12', '13', '14', '15')
-print(separator.join(numtuple))
-
-s1 = 'abc'
-s2 = '123'
-
-print('s1.join(s2):', s1.join(s2))
-print('s2.join(s1):', s2.join(s1))
