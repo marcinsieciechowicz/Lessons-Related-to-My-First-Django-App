6084d9d23ae18cc097eaa9febd0ea3f64880c494
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2019-12-26 00:58:27 +0100

    adding all.py

---
+++ my_exercises_on_builtin_functions/all.py
@@ -0,0 +1,15 @@
+mylist = [True, True, True]
+x = all(mylist)
+print(x)
+
+another_list = [0, 1, 2]
+a = all(another_list)
+print(a)
+
+tuple = (0, True, False)
+b = all(tuple)
+print(b)
+
+my_dict = {0: 'jablko', 1: 'pomaranaczko'}
+c = all(my_dict)
+print(c)
