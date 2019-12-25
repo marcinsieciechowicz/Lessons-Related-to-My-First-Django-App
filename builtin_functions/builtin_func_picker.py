26dd583ff4b1ec36e74b406c2f85520a375e0ed7
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2019-12-25 01:38:34 +0100

    practicing

--- builtin_functions/builtin_func_picker.py
+++ builtin_functions/builtin_func_picker.py
@@ -1,14 +1,27 @@
-import sqlite3
+# import sqlite3
+#
+# conn = sqlite3.connect('db')
+# with conn:
+#     conn.row_factory = sqlite3.Row
+#     cur = conn.cursor()
+#     sql = 'SELECT name FROM builtin_funcs WHERE learned_on IS NULL ORDER BY RANDOM() LIMIT 1 '
+#     cur.execute(sql)
+#     row = cur.fetchone()
+#     random_builtin_func = row['name']
+#     url = 'https://docs.python.org/3/library/functions.html#' + random_builtin_func.replace('()', '')
+#     print(url)
+#     sql = 'UPDATE builtin_funcs SET learned_on = DATE() WHERE name = ?'
+#     cur.execute(sql, [random_builtin_func])
 
-conn = sqlite3.connect('db')
-with conn:
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
-    sql = 'SELECT name FROM builtin_funcs WHERE learned_on IS NULL ORDER BY RANDOM() LIMIT 1 '
-    cur.execute(sql)
-    row = cur.fetchone()
-    random_builtin_func = row['name']
-    url = 'https://docs.python.org/3/library/functions.html#' + random_builtin_func.replace('()', '')
-    print(url)
-    sql = 'UPDATE builtin_funcs SET learned_on = DATE() WHERE name = ?'
-    cur.execute(sql, [random_builtin_func])
+numlist = ['12', '13', '14', '15']
+separator = ', '
+print(separator.join(numlist))
+
+numtuple = ('12', '13', '14', '15')
+print(separator.join(numtuple))
+
+s1 = 'abc'
+s2 = '123'
+
+print('s1.join(s2):', s1.join(s2))
+print('s2.join(s1):', s2.join(s1))
---
+++ my_exercises_on_builtin_functions/sum.py
@@ -0,0 +1,25 @@
+numlist = ['12', '13', '14', '15']
+separator = ', '
+print(separator.join(numlist))
+
+numtuple = ('12', '13', '14', '15')
+print(separator.join(numtuple))
+
+s1 = 'abc'
+s2 = '123'
+
+print('s1.join(s2):', s1.join(s2))
+print('s2.join(s1):', s2.join(s1))
+
+test = {'20', '1', '3'}
+s = ', '
+
+print(s.join(test))
+
+test = {'Python', 'Java', 'Ruby'}
+s = '->->'
+print(s.join(test))
+
+t_test = {'mat':1, 'that':2}
+sep = '->'
+print(sep.join(t_test))
