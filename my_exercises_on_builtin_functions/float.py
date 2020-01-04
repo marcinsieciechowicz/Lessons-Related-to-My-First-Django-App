4ee7af2e215595c3ae1fa62de47cc0d87e3ef86a
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2020-01-04 12:35:30 +0100

    adding float to github

---
+++ my_exercises_on_builtin_functions/float.py
@@ -0,0 +1,9 @@
+print(float(10))
+print(float(11.22))
+print(float('   -15.345\n'))
+# print(float('abc')) can't convert string to float!
+
+print(float('NaN'))
+print(float('nan'))
+print(float('inf'))
+print(float())
