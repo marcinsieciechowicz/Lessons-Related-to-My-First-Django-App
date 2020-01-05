5ffa64a3bce180961c99d905238d672c7c91938c
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2020-01-05 13:36:39 +0100

    adding frozenset

---
+++ my_exercises_on_builtin_functions/frozenset.py
@@ -0,0 +1,13 @@
+# 1st example
+num = (1,2,3,4,5,6,7)
+
+fnum = frozenset(num)
+print('frozenset object is:', fnum)
+# 2nd example
+
+my_dict = {'name': 'Marcin', 'age': 27, 'studies': 'WSB', 'best in': 'chess'}
+
+freeze = frozenset(my_dict)
+
+print('the frozenset is:', freeze)
+
