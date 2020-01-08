eca780f35ae69ab832573a593bb03379186d8bf2
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2020-01-08 21:42:59 +0100

    adding range()

---
+++ my_exercises_on_builtin_functions/range.py
@@ -0,0 +1,12 @@
+# range (start, stop[, step])
+
+# for i in range(6):
+    # print(i, end=',')
+
+for a in range(2, 10):
+    print(a, end=', ')
+
+for c in range (20, 30, 2):
+    print(c, end=', ')
+
+
