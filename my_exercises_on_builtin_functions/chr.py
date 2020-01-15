bcd4087c2a427eaca9a9f984672073e5981ddca6
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2020-01-16 00:46:23 +0100

    adding chr() (mysterious function for me, where the fuck can I use it and for what?)

---
+++ my_exercises_on_builtin_functions/chr.py
@@ -0,0 +1,9 @@
+# syntax: chr(i) (takes only one parameter - integer)
+
+print(chr(12))
+
+# print(chr(szuszwol)) error - must be int
+
+# print(chr(-1))  mistake - out of range
+
+print(chr(97))
--- my_exercises_on_builtin_functions/open.py
+++ my_exercises_on_builtin_functions/open.py
@@ -1,3 +1,3 @@
-a = open('C:/Users/Acer/Desktop/cv2019.pdf', 'r')
+a = open('C:/Users/Acer/Desktop/cv2019.pdf', 'rb')
 
 print(a.read(5))
