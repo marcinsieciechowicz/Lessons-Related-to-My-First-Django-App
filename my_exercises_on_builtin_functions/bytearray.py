56f4f82edd4cac72c94e5f0c7b9d618772734004
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2020-01-21 01:27:08 +0100

    adding bytearray() and len()

---
+++ my_exercises_on_builtin_functions/bytearray.py
@@ -0,0 +1,12 @@
+a = 'jakas wartosc'
+
+argh = bytearray(a, 'utf-8')
+print(argh)
+
+size = 7
+arghh = bytearray(size)
+print(arghh)
+
+Lista = [1, 2, 3, 4]
+listow = bytearray(Lista)
+print(listow)
---
+++ my_exercises_on_builtin_functions/len.py
@@ -0,0 +1,7 @@
+a_list = []
+
+print(a_list, 'the lenght of it is: ', len(a_list))
+
+
+print(len('szuszwol'))
+
