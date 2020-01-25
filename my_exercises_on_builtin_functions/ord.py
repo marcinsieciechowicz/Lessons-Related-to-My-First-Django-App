b0c867778c44e3f9a992cd91fd1b5fa04280a79e
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2020-01-25 23:07:27 +0100

    adding ord() and print()

---
+++ my_exercises_on_builtin_functions/ord.py
@@ -0,0 +1,5 @@
+# returns the integer, which represents the string, for instance, easy function
+
+print(ord('s'))
+
+print(ord('$'))
---
+++ my_exercises_on_builtin_functions/print.py
@@ -0,0 +1,9 @@
+# easy function, not much to write about it :D
+
+print('szuszwol')
+
+kto_jest_szuszwolem = 'szuszwol'
+
+print(kto_jest_szuszwolem)
+
+print('szuszwol', kto_jest_szuszwolem, sep=' yy_okkk ')
