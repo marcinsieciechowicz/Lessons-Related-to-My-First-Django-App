3534351bc3d2eac140c74aa3034250c3e5cbdd6a
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2020-01-26 15:47:25 +0100

    adding max()

---
+++ my_exercises_on_builtin_functions/max.py
@@ -0,0 +1,14 @@
+# can be used in two forms: to find the largest item in iterable or to find the largest between two or more objects
+
+number = [1, 2, 3, 4, 5, 6]
+
+largest_num = max(number)
+
+print('largest is: ', largest_num)
+
+
+larg_str = ['szuszwol', 'szuszwolini', 'szuszwolollol']
+
+larg_string = max(larg_str)
+
+print('largest string issss: ', larg_string)
