a7fc8325c10b6d762753f187c3bba0f5d66e5729
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2020-01-28 15:04:41 +0100

    adding iter() and eval()

---
+++ my_exercises_on_builtin_functions/eval.py
@@ -0,0 +1,10 @@
+# syntax: eval(expression, globals and locals are optional)
+
+# example:
+from math import *
+argh = 1
+
+print(eval('argh + 1'))
+
+print(eval('dir(), {}'))
+
---
+++ my_exercises_on_builtin_functions/iter.py
@@ -0,0 +1,16 @@
+# syntax: iter(object, sentinel(optional))
+
+costam = ['a', 'b', 'c', 'd', 'e']
+
+hehe = iter(costam)
+
+print(next(hehe))
+print(next(hehe))
+print(next(hehe))
+print(next(hehe))
+print(next(hehe))
+print(next(hehe))
+print(next(hehe))
+print(next(hehe))
+
+
