665f10b95b8d8d0f141729d38bc00568f671ebe7
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2020-03-17 13:43:42 +0100

    adding the rest of funcs to githubt

---
+++ my_exercises_on_builtin_functions/ascii.py
@@ -0,0 +1,7 @@
+some_text = 'marcin is no a szuszwol'
+
+print(ascii(some_text))
+
+jakis_tekst = 'marcin óóóó'
+
+print(ascii(jakis_tekst))
---
+++ my_exercises_on_builtin_functions/sorted.py
@@ -0,0 +1,11 @@
+# syntax: sorted(iterable, key=none, reverse=False)
+
+# examples:
+
+some_list = ['m','a','r','c','i','n']
+
+print(sorted(some_list))
+
+moj_string = 'Marcin'
+
+print(sorted(moj_string))
